10d78349b192e9e4a71f1c01eaaff9f130e7ac10
Samuel Holland <samuel@sholland.org>
2021-10-24 13:48:32 -0500

    Bluetooth: hci_bcm: Silence incorrect warning
    Signed-off-by: Samuel Holland <samuel@sholland.org>

--- drivers/bluetooth/hci_bcm.c
+++ drivers/bluetooth/hci_bcm.c
@@ -1467,7 +1467,7 @@ static int bcm_serdev_probe(struct serdev_device *serdev)
 	if (err)
 		return err;
 
-	if (!bcmdev->shutdown) {
+	if (!bcmdev->shutdown && !bcmdev->reset) {
 		dev_warn(&serdev->dev,
 			 "No reset resource, using default baud rate\n");
 		bcmdev->oper_speed = bcmdev->init_speed;
