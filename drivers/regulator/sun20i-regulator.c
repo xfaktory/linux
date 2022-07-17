4526b1ab5dd4460badb20e112b84fe22c00872d4
Samuel Holland <samuel@sholland.org>
2022-07-17 11:46:52 -0500

    regulator: sun20i: Add support for Allwinner D1 LDOs
    D1 contains two pairs of LDOs. Since they have similar bindings, and
    they always exist together, put them in a single driver.
    
    The analog LDOs are relatively boring, with a single linear range. Their
    one quirk is that a bandgap reference must be calibrated for them to
    produce the correct voltage.
    
    The system LDOs have the complication that their voltage step is not an
    integer, so a custom .list_voltage is needed to get the rounding right.
    
    Series-changes: 2
     - Use decimal numbers for .n_voltages instead of field widths
     - Get the regmap from the parent device instead of a property/phandle
    
    Series-changes: 3
     - Adjust control flow in sun20i_regulator_get_regmap() for clarity
    
    Reviewed-by: Heiko Stuebner <heiko@sntech.de>
    Tested-by: Heiko Stuebner <heiko@sntech.de>
    Signed-off-by: Samuel Holland <samuel@sholland.org>

--- drivers/regulator/sun20i-regulator.c
+++ drivers/regulator/sun20i-regulator.c
@@ -5,16 +5,87 @@
 
 #include <linux/mfd/syscon.h>
 #include <linux/module.h>
+#include <linux/nvmem-consumer.h>
 #include <linux/of_device.h>
 #include <linux/platform_device.h>
 #include <linux/regmap.h>
 #include <linux/regulator/driver.h>
 
+#define SUN20I_POWER_REG		0x348
+
 #define SUN20I_SYS_LDO_CTRL_REG		0x150
 
 struct sun20i_regulator_data {
+	int				(*init)(struct device *dev,
+						struct regmap *regmap);
 	const struct regulator_desc	*descs;
 	unsigned int			ndescs;
+};
+
+static int sun20i_d1_analog_ldos_init(struct device *dev, struct regmap *regmap)
+{
+	u8 bg_trim;
+	int ret;
+
+	ret = nvmem_cell_read_u8(dev, "bg_trim", &bg_trim);
+	if (ret)
+		return dev_err_probe(dev, ret, "Failed to get bg_trim value\n");
+
+	/* The default value corresponds to 900 mV. */
+	if (!bg_trim)
+		bg_trim = 0x19;
+
+	return regmap_update_bits(regmap, SUN20I_POWER_REG,
+				  GENMASK(7, 0), bg_trim);
+}
+
+static const struct regulator_ops sun20i_d1_analog_ldo_ops = {
+	.list_voltage		= regulator_list_voltage_linear,
+	.map_voltage		= regulator_map_voltage_linear,
+	.set_voltage_sel	= regulator_set_voltage_sel_regmap,
+	.get_voltage_sel	= regulator_get_voltage_sel_regmap,
+	.enable			= regulator_enable_regmap,
+	.disable		= regulator_disable_regmap,
+	.is_enabled		= regulator_is_enabled_regmap,
+};
+
+static const struct regulator_desc sun20i_d1_analog_ldo_descs[] = {
+	{
+		.name		= "aldo",
+		.supply_name	= "vdd33",
+		.of_match	= "aldo",
+		.ops		= &sun20i_d1_analog_ldo_ops,
+		.type		= REGULATOR_VOLTAGE,
+		.owner		= THIS_MODULE,
+		.n_voltages	= 8,
+		.min_uV		= 1650000,
+		.uV_step	= 50000,
+		.vsel_reg	= SUN20I_POWER_REG,
+		.vsel_mask	= GENMASK(14, 12),
+		.enable_reg	= SUN20I_POWER_REG,
+		.enable_mask	= BIT(31),
+	},
+	{
+		.name		= "hpldo",
+		.supply_name	= "hpldoin",
+		.of_match	= "hpldo",
+		.ops		= &sun20i_d1_analog_ldo_ops,
+		.type		= REGULATOR_VOLTAGE,
+		.owner		= THIS_MODULE,
+		.n_voltages	= 8,
+		.min_uV		= 1650000,
+		.uV_step	= 50000,
+		.vsel_reg	= SUN20I_POWER_REG,
+		.vsel_mask	= GENMASK(10, 8),
+		.enable_reg	= SUN20I_POWER_REG,
+		.enable_mask	= BIT(30),
+	},
+};
+
+static const struct sun20i_regulator_data sun20i_d1_analog_ldos = {
+	.init	= sun20i_d1_analog_ldos_init,
+	.descs	= sun20i_d1_analog_ldo_descs,
+	.ndescs	= ARRAY_SIZE(sun20i_d1_analog_ldo_descs),
 };
 
 /* regulator_list_voltage_linear() modified for the non-integral uV_step. */
@@ -107,6 +178,7 @@ static int sun20i_regulator_probe(struct platform_device *pdev)
 	struct device *dev = &pdev->dev;
 	struct regulator_config config;
 	struct regmap *regmap;
+	int ret;
 
 	data = of_device_get_match_data(dev);
 	if (!data)
@@ -115,6 +187,12 @@ static int sun20i_regulator_probe(struct platform_device *pdev)
 	regmap = sun20i_regulator_get_regmap(dev);
 	if (IS_ERR(regmap))
 		return dev_err_probe(dev, PTR_ERR(regmap), "Failed to get regmap\n");
+
+	if (data->init) {
+		ret = data->init(dev, regmap);
+		if (ret)
+			return ret;
+	}
 
 	config = (struct regulator_config) {
 		.dev	= dev,
@@ -134,6 +212,10 @@ static int sun20i_regulator_probe(struct platform_device *pdev)
 }
 
 static const struct of_device_id sun20i_regulator_of_match[] = {
+	{
+		.compatible = "allwinner,sun20i-d1-analog-ldos",
+		.data = &sun20i_d1_analog_ldos,
+	},
 	{
 		.compatible = "allwinner,sun20i-d1-system-ldos",
 		.data = &sun20i_d1_system_ldos,
